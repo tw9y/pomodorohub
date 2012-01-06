5eafde56240b98b08b40c1d6d0d4a27132d24615
Markus Ullmark <ullmark@gmail.com>
2012-01-06 15:56:20 +0100

    new structure

---
+++ models/pomodoro.rb
@@ -0,0 +1,8 @@
+require 'mongo/model'
+
+class Pomodoro
+  inherit Mongo::Model
+  collection :pomodoros
+
+#  attr_accessor
+end
---
+++ specs/test_helper.rb
@@ -0,0 +1,5 @@
+ENV['RACK_ENV'] = 'test'
+
+require 'rubygems'
+require 'rack/test'
+require_relative '../app'
